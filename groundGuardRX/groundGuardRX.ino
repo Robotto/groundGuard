4ea007a6e10f58bfed8b9a5759fca10042457992
robotto <mark@moore.dk>
2016-10-28 13:43:48 +0200

    initial RX test code, just dumps to serial.

---
+++ groundGuardRX/groundGuardRX.ino
@@ -0,0 +1,20 @@
+//groundGuardRX.ino
+
+static int vibroPin = D1;
+
+void setup() {
+	Serial.begin(115200);
+	Serial.swap(); //RX is now on D7
+
+	Serial.setTimeout(100); //how long to wait when parsing data from serial port
+}
+
+float height=100;
+
+void loop() {
+
+	//height = Serial.parseFloat();
+
+	if(Serial.available()) Serial.write(Serial.read); //TEST CODE
+
+}
