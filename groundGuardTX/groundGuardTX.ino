ae69fd71271366f669c2804e01dad211e62f8445
robotto <mark@moore.dk>
2016-11-18 09:06:43 +0100

    now on proMicro boards with joint use of serial1

--- groundGuardTX/groundGuardTX.ino
+++ groundGuardTX/groundGuardTX.ino
@@ -1,5 +1,4 @@
 //groundGuardTX.ino - By Mark Moore - October 2016
-
 #define packetLength 16
 const float pi = 3.14159;
 
@@ -14,7 +13,7 @@ LIDARLite Lidar;
 //FC:
 byte fifo0=0, fifo1=0, fifo2=0;
 
-int16_t pitchAngle/*, rollAngle, headingAngle*/;
+int16_t pitchAngle=45/*, rollAngle, headingAngle*/;
 int16_t lastPitchAngle;
 float pitchRadians;
 byte fcRX;
@@ -23,10 +22,8 @@ unsigned long attitudeFrameCounter=0;
 
 void setup() {
   //using the same UART for RXing from flight controller and TXing to HC-12:
-	Serial.begin(115200); //RX: GPIO3, TX: GPIO1
-  Serial.swap(); //RX:Pin D7/GPIO13 TX:Pin D8/GPIO15
-
-
+	Serial1.begin(115200); //RX: GPIO3, TX: GPIO1
+  Serial.begin(115200);
 	Lidar.begin(0, true); // Set configuration to default and I2C to 400 kHz
 
 	Lidar.write(0x02, 0x0d);       //Maximum acquisition count of 0x0d. (default is 0x80) (limits effective range to about half of max)
@@ -35,33 +32,39 @@ void setup() {
 
 	delay(500);
 
-	Serial.println("groundGuard online.");
-	Serial.println("Hello basestation my old friend,");
-	Serial.println("I've come online to talk to you again.");
+	Serial1.println("groundGuard online.");
+	Serial1.println("Hello basestation my old friend,");
+	Serial1.println("I've come online to talk to you again.");
 }
 
 void loop() {
 
-  //distance = distanceFast(true); // Take a measurement with receiver bias correction and print to serial terminal
-  distance = 100; //testdata
+
+
+  distance = distanceFast(true); // Take a measurement with receiver bias correction and print to serial terminal
+  //distance = 100; //testdata
    // Take 99 measurements without receiver bias correction and print to serial terminal
   //for(int i = 0; i < 99; i++) Serial1.println(distanceFast(false));
   
   //recalculate height if either pitchAngle or distance measurement has changed:
   if(pitchAngle!=lastPitchAngle || distance!=lastDistance) {
   	
+    lastDistance=distance;
+    lastPitchAngle=pitchAngle;
+
   	pitchRadians = (float)pitchAngle*pi/180;
   	float height = distance*cos(pitchRadians);
   	
-  	if(Serial.availableForWrite()>=packetLength) Serial.print(height);  //Transmit if there is room in the serial TX buffer  	
+  	/*if(Serial1.availableForWrite()>=packetLength) {*/ Serial1.println(height); // }  //Transmit if there is room in the serial TX buffer  	
+    /*if(Serial.availableForWrite()>=packetLength)  {*/ Serial.print("Frame#: "); Serial.println(attitudeFrameCounter); Serial.println(height);   // } //Transmit if there is room in the serial TX buffer   
   }
 
 
-if(Serial.available()){
+if(Serial1.available()){
 	//3 byte fifo buffer:
 	fifo0 = fifo1;
 	fifo1 = fifo2;
-	fifo2 = Serial.read();
+	fifo2 = Serial1.read();
 
 	if(fifo0=='$' && fifo1=='T' && fifo2=='A') {//the next 6 rx bytes are the attitude frame
 
@@ -72,9 +75,9 @@ if(Serial.available()){
 	//recieve and 'decode' the first 2 bytes only, into the pitch uint16_t with little endianness:
 		for(int i = 0; i<2 ;i++) {
 
-			while(!Serial.available()); //wait for byte
+			while(!Serial1.available()); //wait for byte
 
-			fcRX=Serial.read();
+			fcRX=Serial1.read();
 
 			switch (i) {
 		    	case 0: //first pitch byte (binary: xxxx xxxx)
@@ -84,23 +87,23 @@ if(Serial.available()){
 		    	case 1:	//second pitch byte (binary: yyyy yyyy)
 		      		pitchAngle |= fcRX<<8; //pitch = yyyy yyyy xxxx xxxx 
 		      		break;
-/*
-			case 2:	//first roll byte (binary: zzzz zzzz)
-		      rollAngle = rx; //roll = 0000 0000 zzzz zzzz
-		      break;
 
-		    case 3:	//second roll byte (binary: ææææ ææææ)
-		      rollAngle |= rx<<8; //roll = ææææ ææææ zzzz zzzz
-		      break;
+			// case 2:	//first roll byte (binary: zzzz zzzz)
+		 //      rollAngle = rx; //roll = 0000 0000 zzzz zzzz
+		 //      break;
 
-		    case 4:	//first heading byte (binary: øøøø øøøø)
-		      headingAngle = rx; //heading = 0000 0000 øøøø øøøø
-		      break;
+		 //    case 3:	//second roll byte (binary: ææææ ææææ)
+		 //      rollAngle |= rx<<8; //roll = ææææ ææææ zzzz zzzz
+		 //      break;
 
-		    case 5: //second heading byte (binary: åååå åååå)
-		      headingAngle |= rx<<8; //heading = åååå åååå øøøø øøøø
-		      break;
-*/
+		 //    case 4:	//first heading byte (binary: øøøø øøøø)
+		 //      headingAngle = rx; //heading = 0000 0000 øøøø øøøø
+		 //      break;
+
+		 //    case 5: //second heading byte (binary: åååå åååå)
+		 //      headingAngle |= rx<<8; //heading = åååå åååå øøøø øøøø
+		 //      break;
+
 		    default: //shouldn't happen...
 		      break;
 				}
