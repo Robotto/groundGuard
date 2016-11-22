75ee999b3df9918df5f07691957ee2455ce33e02
robotto <mark@moore.dk>
2016-11-22 22:11:24 +0100

    fixed merge fuckup

--- groundGuardRX/groundGuardRX.ino
+++ groundGuardRX/groundGuardRX.ino
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+//groundGuardRX.ino
 const unsigned int vibratorPin = 5;
 void setup()
 {
@@ -11,28 +11,10 @@ void setup()
 
 	pinMode(vibratorPin,OUTPUT);
 
-=======
-//groundGuardRX.ino
-#include "ESP8266WiFi.h" //ironically, this is included to allow for disabling wifi..
-
-static int vibroPin = D3;
-
-void setup() {
-	WiFi.forceSleepBegin();                  // turn off ESP8266 RF
-	delay(1);  
-
-	//while(!Serial); //wait for PC connection
-
-	Serial.begin(115200);
-	//Serial.swap(); //RX is now on D7
-
-	Serial.setTimeout(100); //how long to wait when parsing data from serial port
->>>>>>> master
 }
 
 float height=100;
 
-<<<<<<< HEAD
 void loop()
 {
 	height = Serial1.parseFloat();
@@ -40,12 +22,4 @@ void loop()
 
 	if(height<10) digitalWrite(vibratorPin, HIGH);
 	else digitalWrite(vibratorPin, LOW);
-=======
-void loop() {
-
-	//height = Serial.parseFloat();
-
-	if(Serial.available()) Serial.write(Serial.read()); //TEST CODE
-
->>>>>>> master
 }
