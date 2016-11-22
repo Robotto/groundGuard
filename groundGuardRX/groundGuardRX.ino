ea93561556d2582dd6c2ec028418004cf52841f5
robotto <mark@moore.dk>
2016-11-22 22:01:50 +0100

    added groundguardRX with hardcoded warning height at 10cm

---
+++ groundGuardRX/groundGuardRX.ino
@@ -0,0 +1,24 @@
+const unsigned int vibratorPin = 5;
+void setup()
+{
+
+	//while(!Serial);
+	Serial.begin(115200);
+	Serial1.begin(115200);
+
+	Serial1.setTimeout(5000); //ms
+
+	pinMode(vibratorPin,OUTPUT);
+
+}
+
+float height=100;
+
+void loop()
+{
+	height = Serial1.parseFloat();
+	Serial.println(height);
+
+	if(height<10) digitalWrite(vibratorPin, HIGH);
+	else digitalWrite(vibratorPin, LOW);
+}
